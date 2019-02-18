5809583830422877aa3019495141ad7aa9582de1
UnderD0g <underd0g.3003@gmail.com>
2019-02-18 19:03:26 -0300

    adding html

--- js/script.js
+++ js/script.js
@@ -16,14 +16,66 @@ FSJS project 1 - A Random Quote Generator
       quote object.
 ***/
 var quotes = [
-    {
-       quote: 'Macintosh',
-       source: 'Red'
+     {
+        quote:'Whatever the mind of man can conceive and believe, it can achieve',
+        source:'Napoleon Hill',
+        citation: 'https://www.forbes.com/sites/kevinkruse/2013/05/28/inspirational-quotes/#4a2c60f36c7a',
+        year:'1923'
     },
     {
-       quote: 'Fuji',
-       source: 'Green'
+        quote:' Strive not to be a success, but rather to be of value',
+        source:'Albert Einstein',
+        citation: 'https://www.forbes.com/sites/kevinkruse/2013/05/28/inspirational-quotes/#4a2c60f36c7a',
+        year:''
     },
+    {
+        quote:'You miss 100% of the shots you don’t take.',
+        source:'Wayne Gretzky',
+        citation: 'https://www.forbes.com/sites/kevinkruse/2013/05/28/inspirational-quotes/#4a2c60f36c7a',
+        year:''
+    },
+    {
+        quote:'Every strike brings me closer to the next home run',
+        source:'Babe Ruth',
+        citation: 'https://www.forbes.com/sites/kevinkruse/2013/05/28/inspirational-quotes/#4a2c60f36c7a',
+        year:''
+    },
+    {
+        quote:'Eighty percent of success is showing up.',
+        source:'Woody Allen',
+        citation: 'https://www.forbes.com/sites/kevinkruse/2013/05/28/inspirational-quotes/#4a2c60f36c7a',
+        year:''
+    },
+    {
+        quote:'Winning isn’t everything, but wanting to win is.',
+        source:'Vince Lombardi',
+        citation: 'https://www.forbes.com/sites/kevinkruse/2013/05/28/inspirational-quotes/#4a2c60f36c7a',
+        year:''
+    },
+    {
+        quote:'I’ve missed more than 9000 shots in my career. I’ve lost almost 300 games. 26 times I’ve been trusted to take the game winning shot and missed. I’ve failed over and over and over again in my life. And that is why I succeed.',
+        source:'Michael Jordan',
+        citation: 'https://www.forbes.com/sites/kevinkruse/2013/05/28/inspirational-quotes/#4a2c60f36c7a',
+        year:''
+    },
+    {
+        quote:'Only I can change my life. No one can do it for me',
+        source:'Carol Burnett',
+        citation: 'https://www.brainyquote.com',
+        year:'1933'
+    },
+    {
+        quote:'Always do your best. What you plant now, you will harvest later.',
+        source:'Og Mandino',
+        citation: 'https://www.brainyquote.com',
+        year:'1923'
+    },
+    {
+        quote:'Life is about making an impact, not making an income',
+        source:'Kevin Kruse',
+        citation: 'http://www.forbes.com/sites/kevinkruse/2013/05/28/inspirational-quotes/',
+        year:'2016'
+    }
 ];
 
 
@@ -35,11 +87,18 @@ var quotes = [
      `quotes` array.
 ***/
 function getRandomQuote(){
-  let randomNumber = Math.random() * (quotes.length-0) + 0;
+  let randomNumber = Math.random() * (quotes.length);
   return quotes[Math.floor(randomNumber)];
 }
 
-
+function createHtmlFromQuote(quote){
+    var html = '<p class="quote">' + quote['quote'] + '</p>';
+    html += '<p class="source">' + quote['source'] + '<br>';
+    html += '<span class="citation">'+ quote['citation'] + '</span> <br>';
+    html += '<span class="year" ' + quote['year'] + '</span>';
+    html += '</p>';
+    return html;
+}
 
 /***
   Create the `printQuote` function to: 
@@ -51,8 +110,8 @@ function getRandomQuote(){
    - set the `innerHTML` of the `quote-box` div to the HTML string. 
 ***/
 function printQuote(){
-  let quote = getRandomQuote();
-  alert(quote.quote);
+  var quote = getRandomQuote();
+  document.getElementById('quote-box').innerHTML = createHtmlFromQuote(quote);
 
 }
 
